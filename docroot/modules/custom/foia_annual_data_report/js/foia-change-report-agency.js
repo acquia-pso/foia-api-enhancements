5c78f6daf29602f54292ebed7d0e0d123dd76776
Jesse Day <jday@isovera.com>
2019-10-24 16:28:26 -0600

    FOIA-202: Add documentation to js function.

--- docroot/modules/custom/foia_annual_data_report/js/foia-change-report-agency.js
+++ docroot/modules/custom/foia_annual_data_report/js/foia-change-report-agency.js
@@ -10,6 +10,9 @@
      * The change event doesn't fire for the autocomplete field in IE11.  To work around this,
      * this method listens for the blur event on the field, checks if the field value has
      * changed, and triggers a refresh in all browsers if it has.
+     *
+     * @see foia_annual_data_report_ajax_existing_node()
+     * @see foia_annual_data_report_ajax_new_node()
      */
     triggerNodeRefreshOnUpdate: function() {
       drupalSettings.foiaReportAgencyInitialValue = $('#edit-field-agency-0-target-id').val();
